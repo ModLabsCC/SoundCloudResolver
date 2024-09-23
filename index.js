f54c3bb4b490fb7ff3c0bd549a315183db03eaa7
Cranberry <20140992+cranberry3148@users.noreply.github.com>
2024-09-23 21:30:08 +0200

    uhm, yes

--- index.js
+++ index.js
@@ -25,48 +25,98 @@ async function serveAsset(event) {
                 return response;
             }
 
+            let currentUrl = "https://w.soundcloud.com/player/";
+
+            console.log(currentUrl);
             let req = await fetch("https://w.soundcloud.com/player/");
             req = await req.text();
             let jsFile = req.split("sndcdn.com/widget-9-")[1].split('.js')[0];
-            jsFile = `https://widget.sndcdn.com/widget-9-${jsFile}.js`
-            req = await fetch(jsFile);
+
+            currentUrl = `https://widget.sndcdn.com/widget-9-${jsFile}.js`;
+            console.log(currentUrl);
+            req = await fetch(currentUrl);
             req = await req.text();
 
             let soundCloudID = req.split("client_id")[1].split('"')[1];
 
-            await fetch("https://api-v2.soundcloud.com/resolve?url=https://soundcloud.com" + url.pathname + "&client_id=" + soundCloudID).then(async function (tmpResp) {
+            currentUrl = "https://api-v2.soundcloud.com/resolve?url=https://soundcloud.com" + url.pathname + "&client_id=" + soundCloudID;
+            console.log(currentUrl);
+            await fetch(currentUrl).then(async function (tmpResp) {
 
                 let tmpJson = await tmpResp.json();
                     switch (tmpJson.kind) {
                         case "track":
 
                             let streamUrl = "";
+                            console.log(tmpJson);
                             for(const transocing of tmpJson.media.transcodings) {
-                                if(transocing.format.protocol === "progressive") {
+                                if(transocing.format.mime_type === "audio/mpeg") {
                                     streamUrl = transocing.url;
                                     break;
                                 }
                             }
 
+                            if(streamUrl === "") {
+                                response = new Response("", {statusText: "Couldn't find HLS stream url", status: 422})
+                                break;
+                            }
+
                             streamUrl = streamUrl + "?client_id=" + soundCloudID;
+
+                            console.log(streamUrl);
                             streamUrl = await fetch(streamUrl);
                             streamUrl = await streamUrl.json();
                             streamUrl = streamUrl.url;
 
-                            var audio = await fetch(streamUrl);
-                            let {readable, writable} = new TransformStream();
-                            const contentLength = audio.headers.get("content-length");
-                            audio.body.pipeTo(writable)
-                            var headers = audio.headers;
+                            // Implementing the Fetch HLS playlist, concatenate MP3 parts, and serve as a single MP3 file
+                            let playlistResponse = await fetch(streamUrl);
+                            let playlistText = await playlistResponse.text();
+
+                            // Parse the playlist to get MP3 part URLs
+                            let lines = playlistText.split('\n');
+                            let mediaUrls = [];
+                            for (let line of lines) {
+                                line = line.trim();
+                                if (line && !line.startsWith('#')) {
+                                    let mediaUrl = new URL(line, streamUrl).toString();
+                                    mediaUrls.push(mediaUrl);
+                                }
+                            }
+
+                            // Create a TransformStream to pipe the MP3 parts
+                            let { readable, writable } = new TransformStream();
+
+                            (async () => {
+                                const writer = writable.getWriter();
+
+                                try {
+                                    for (let mediaUrl of mediaUrls) {
+                                        const partResponse = await fetch(mediaUrl);
+                                        if (!partResponse.ok) {
+                                            throw new Error(`Failed to fetch MP3 part: ${mediaUrl}`);
+                                        }
+                                        const reader = partResponse.body.getReader();
+                                        while (true) {
+                                            const { done, value } = await reader.read();
+                                            if (done) break;
+                                            await writer.write(value);
+                                        }
+                                    }
+                                    await writer.close();
+                                } catch (error) {
+                                    console.error(error);
+                                    await writer.abort(error);
+                                }
+                            })();
+
+                            var headers = {};
                             headers['cache-control'] = "public, max-age=14400";
                             headers['Access-Control-Allow-Origin'] = "*";
                             headers['Content-Type'] = "audio/mpeg";
                             headers['Content-Encoding'] = "deflate";
-                            headers['Content-Length'] = contentLength;
                             headers['Content-Disposition'] = `filename="${tmpJson.user.username} - ${tmpJson.title}.mp3"`;
                             headers['X-Content-Duration'] = tmpJson.duration / 1000;
                             response = new Response(readable, {headers});
-                            event.waitUntil(cache.put(event.request, response.clone()));
                             break;
                         case "playlist":
                             let tracks = tmpJson.tracks;
@@ -88,7 +138,6 @@ async function serveAsset(event) {
                             };
                             response = new Response(outStr, {headers, statusText: "OK", status: 200});
                             response.status = 200;
-                            event.waitUntil(cache.put(event.request, response.clone()));
                             break;
                         default:
                             var headers = {
